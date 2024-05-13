8572e2fa3c9a19cfb23078c8de63521a249ea2f5
sogawa-yk <yuki.sogawa@oracle.com>
2024-05-13 05:22:48 +0000

    個人ごとにシェル起動機能追加

--- env/backend/app/k8s_client.py
+++ env/backend/app/k8s_client.py
@@ -17,36 +17,46 @@ init_k8s_client()
 
 def create_pod(pod_name):
     # マニフェストファイルからPodを作成
-    template_pod_manifest_path = '/config/web-shell.yaml'
-    template_svc_manifest_path = '/config/web-shell-service.yaml'
+    template_pod_manifest_path = '/config/web-shell-template.yaml'
+    template_svc_manifest_path = '/config/web-shell-service-template.yaml'
+    template_ingress_manifest_path = '/config/ingress-template.yaml'
     output_path = '/config/'
     try:
+        # Podマニフェスト生成
         with open(template_pod_manifest_path, 'r') as file:
             template = file.read()
         pod_manifest_yaml = template.format(pod_name=pod_name)
-        
+        pod_manifest_path = output_path + pod_name + '.yaml'
+        with open(pod_manifest_path, 'w') as file:
+            file.write(pod_manifest_yaml)
+
+        # Serviceマニフェスト生成
         with open(template_svc_manifest_path, 'r') as file:
             template = file.read()
         svc_manifest_yaml = template.format(pod_name=pod_name)
-
-        pod_manifest_path = output_path + pod_name + '.yaml'
         svc_manifest_path = output_path + pod_name + '-service.yaml'
-        
-        with open(pod_manifest_path, 'w') as file:
-            file.write(pod_manifest_yaml)
-        
         with open(svc_manifest_path, 'w') as file:
             file.write(svc_manifest_yaml)
 
-        utils.create_from_yaml(client.ApiClient(), yaml_file=pod_manifest_path, namespace='default')
-        utils.create_from_yaml(client.ApiClient(), yaml_file=svc_manifest_path, namespace='default')
+        # Ingressマニフェストの生成
+        with open(template_ingress_manifest_path, 'r') as file:
+            template = file.read()
+        ingress_manifest_yaml = template.format(pod_name=pod_name)
+        ingress_manifest_path = output_path + pod_name + '-ingress.yaml'
+        with open(ingress_manifest_path, 'w') as file:
+            file.write(ingress_manifest_yaml)        
+
+        # マニフェスト適用
+        utils.create_from_yaml(client.ApiClient(), yaml_file=pod_manifest_path, namespace='se-learning-hub')
+        utils.create_from_yaml(client.ApiClient(), yaml_file=svc_manifest_path, namespace='se-learning-hub')
+        utils.create_from_yaml(client.ApiClient(), yaml_file=ingress_manifest_path, namespace='se-learning-hub')
         return "Pod creation initiated successfully."
     except Exception as e:
         print(f"Failed to create pod: {str(e)}", file=sys.stderr)
         return f"Failed to create pod: {str(e)}"
 
 
-def get_pod_status(pod_name, namespace='default'):
+def get_pod_status(pod_name, namespace='se-learning-hub'):
     # 特定のPodのステータスを取得
     try:
         pod = client.CoreV1Api().read_namespaced_pod(name=pod_name, namespace=namespace)
